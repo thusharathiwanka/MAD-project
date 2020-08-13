b44adbe969ab83d2c4bd7b7cd0c5759713fef4f4
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-08-13 19:55:15 +0530

    home page layout finished

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mad_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 }
