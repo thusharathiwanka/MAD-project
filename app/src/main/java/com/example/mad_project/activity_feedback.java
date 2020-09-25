eb07c2d09476d9bfec9c8b21c1316c98cf2b56a6
Shalitha <shalithalakruwan1988@gmail.com>
2020-09-25 23:22:43 +0530

    Feedback_ui links added

--- app/src/main/java/com/example/mad_project/activity_feedback.java
+++ app/src/main/java/com/example/mad_project/activity_feedback.java
@@ -3,12 +3,22 @@ package com.example.mad_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class activity_feedback extends AppCompatActivity {
+
+    Button btn1,btn2;
+    FloatingActionButton mail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        btn1 = findViewById(R.id.add_feedback);
+        btn2 = findViewById(R.id.back);
+        mail = findViewById(R.id.gomail);
     }
 }
