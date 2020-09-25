6422929bdcc65f09696cb0adc689a94bb4be4f72
Shalitha <shalithalakruwan1988@gmail.com>
2020-09-26 00:22:05 +0530

    Create intent to e-mail

--- app/src/main/java/com/example/mad_project/activity_feedback.java
+++ app/src/main/java/com/example/mad_project/activity_feedback.java
@@ -2,7 +2,9 @@ package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,5 +22,20 @@ public class activity_feedback extends AppCompatActivity {
         btn1 = findViewById(R.id.add_feedback);
         btn2 = findViewById(R.id.back);
         mail = findViewById(R.id.gomail);
+
+
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openNewActivity();
+            }
+        });
+
+
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, activity_contact_via_gmail.class);
+        startActivity(intent);
     }
 }
