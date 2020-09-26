41ec67536d29484b596f87cd3e5b63ddbe292dbd
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-09-26 10:38:16 +0530

    custom toast library added (Toasty)

--- app/src/main/java/com/example/mad_project/SelectFavouritesActivity.java
+++ app/src/main/java/com/example/mad_project/SelectFavouritesActivity.java
@@ -9,8 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
+import es.dmoral.toasty.Toasty;
+
 public class SelectFavouritesActivity extends AppCompatActivity {
     ImageView backBtn;
+    Button registerBtn;
+    ArrayList<String> btnValues = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +24,21 @@ public class SelectFavouritesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select_favourites);
 
         backBtn = findViewById(R.id.back);
+        registerBtn = findViewById(R.id.finishBtn);
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(btnValues.isEmpty()) {
+                    Toasty.error(getApplicationContext(), "Select at least one favourite", Toasty.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -40,5 +56,48 @@ public class SelectFavouritesActivity extends AppCompatActivity {
         view.setBackgroundColor(Color.parseColor("#3583EF"));
         Button btn = (Button) view;
         btn.setTextColor(Color.parseColor("#FFFFFF"));
+
+        int btnID = view.getId();
+        String btnValue = null;
+
+        switch (btnID) {
+            case R.id.javaBtn:
+                btnValue = "Java";
+                break;
+            case R.id.htmlBtn:
+                btnValue = "HTML";
+                break;
+            case R.id.cBtn:
+                btnValue = "C";
+                break;
+            case R.id.cssBtn:
+                btnValue = "CSS";
+                break;
+            case R.id.cPlusPlusBtn:
+                btnValue = "C++";
+                break;
+            case R.id.phpBtn:
+                btnValue = "PHP";
+                break;
+            case R.id.jsBtn:
+                btnValue = "JavaScript";
+                break;
+            case R.id.reactJsBtn:
+                btnValue = "React JS";
+                break;
+            case R.id.pythonBtn:
+                btnValue = "Python";
+                break;
+            case R.id.nodeJsBtn:
+                btnValue = "Node JS";
+                break;
+            case R.id.sqlBtn:
+                btnValue = "SQL";
+                break;
+            default:
+                System.out.println("Something went wrong");
+        }
+        btnValues.add(btnValue);
+        System.out.println(btnValues);
     }
 }
