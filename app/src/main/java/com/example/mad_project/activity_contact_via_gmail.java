0d48211b74b1d101a4c05976e1f5861c10d8bc00
shalithacode <shalithalakruwan1998@gmail.com>
2020-10-02 21:36:37 +0530

    check whether internet connected, before send e-mail

--- app/src/main/java/com/example/mad_project/activity_contact_via_gmail.java
+++ app/src/main/java/com/example/mad_project/activity_contact_via_gmail.java
@@ -2,13 +2,18 @@ package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.Toast;
 
 public class activity_contact_via_gmail extends AppCompatActivity {
     Button btn1;
@@ -26,19 +31,28 @@ public class activity_contact_via_gmail extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i= new Intent(Intent.ACTION_SEND);
-                String main_message = maintxt.getText().toString();
-                String sub_message = subtxt.getText().toString();
-                i.setData(Uri.parse("mailto:"));
-                i.putExtra(i.EXTRA_EMAIL,new String[]{"codelearner@learn.com"});
-                i.putExtra(i.EXTRA_SUBJECT, main_message);
-                i.putExtra(i.EXTRA_TEXT,sub_message);
-                i.setType("text/plane");
-                final Intent chooser=Intent.createChooser(i,"Send mail by this app");
+                ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                        connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
 
-                startActivity(chooser);
-            }
-        });
 
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    String main_message = maintxt.getText().toString();
+                    String sub_message = subtxt.getText().toString();
+                    i.setData(Uri.parse("mailto:"));
+                    i.putExtra(i.EXTRA_EMAIL, new String[]{"codelearner@learn.com"});
+                    i.putExtra(i.EXTRA_SUBJECT, main_message);
+                    i.putExtra(i.EXTRA_TEXT, sub_message);
+                    i.setType("text/plane");
+                    final Intent chooser = Intent.createChooser(i, "Send mail by this app");
+
+                    startActivity(chooser);
+                } else {
+                    Toast.makeText(getApplicationContext(), "internet connection is not available", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(WifiManager.ACTION_PICK_WIFI_NETWORK));
+                }
+
+            } });
+
+        }
     }
-}
