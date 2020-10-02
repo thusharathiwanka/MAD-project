4ff43301bc73e172be130a4d9e1ceb59f31cc5a8
shalithacode <shalithalakruwan1998@gmail.com>
2020-10-02 16:02:40 +0530

    Feedback implementation stared

---
+++ app/src/main/java/com/example/mad_project/AddFeedBack.java
@@ -0,0 +1,48 @@
+package com.example.mad_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class AddFeedBack extends AppCompatActivity {
+
+    private EditText title, desc;
+    private Button add;
+    private FeedBackDbHandler dbHandler;
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_feed_back);
+
+        title = findViewById(R.id.editTextTitle);
+        desc = findViewById(R.id.editTextDescription);
+        add = findViewById(R.id.buttonAdd);
+        context = this;
+
+        dbHandler = new FeedBackDbHandler(context);
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userTitle = title.getText().toString();
+                String userDesc = desc.getText().toString();
+                long started = System.currentTimeMillis();
+
+                FeedBack feedBack = new FeedBack(userTitle,userDesc,started,0);
+                dbHandler.addFeedBack(feedBack);
+
+                startActivity(new Intent(context,FeedBackMainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mad_project/FeedBackMainActivity.java
@@ -0,0 +1,80 @@
+package com.example.mad_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FeedBackMainActivity extends AppCompatActivity {
+
+    private Button add;
+    private ListView listView;
+    private TextView count;
+    Context context;
+    private FeedBackDbHandler dbHandler;
+    private List<FeedBack> feedBacks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_back_main);
+        context = this;
+
+        dbHandler = new FeedBackDbHandler(context);
+        add = findViewById(R.id.add);
+        listView = findViewById(R.id.feedbacklist);
+        count = findViewById(R.id.feedbackcount);
+        feedBacks = new ArrayList<>();
+
+        feedBacks = dbHandler.getAllFeedBacks();
+
+        FeedBackAdapter adapter = new FeedBackAdapter(context,R.layout.single_feedback,feedBacks);
+
+        listView.setAdapter(adapter);
+
+        //get todo counts from the table
+        int countFeedBack = dbHandler.countFeedBack();
+        count.setText("You have "+countFeedBack+" feedbacks");
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context,AddFeedBack.class));
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                final FeedBack feedback = feedBacks.get(position);
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle(feedback.getTitle());
+                builder.setMessage(feedback.getDescription());
+
+                builder.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dbHandler.deleteFeedBack(feedback.getId());
+                        startActivity(new Intent(context,FeedBackMainActivity.class));
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+    }
+}
