dfc01ddb3636b4106aaeab9a17ac16d26d4493af
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-10-04 08:20:19 +0530

    add: test cases added for checking valid email and username

--- app/src/test/java/com/example/mad_project/ExampleUnitTest.java
+++ app/src/test/java/com/example/mad_project/ExampleUnitTest.java
@@ -10,8 +10,29 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    //IT19076362 - Test cases for check valid username and email
+
+    /* User email should be an valid email address
+       It can only contains a-z letters, A-Z letters, 0-9 numbers and @ symbol
+       It must contain @ symbol
+    * */
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void isValidEmail() {
+        boolean result = StudentRegisterActivity.emailValidate("test@gmail.com");
+        assertTrue(result);
+        boolean result1 = StudentRegisterActivity.emailValidate("test");
+        assertFalse(result1);
+    }
+
+    /* User username should be an valid username
+       It can only contains a-z letters, A-Z letters, 0-9 numbers
+       It must contain letters and numbers
+    * */
+    @Test
+    public void isValidUsername() {
+        boolean result = StudentRegisterActivity.usernameValidate("test01");
+        assertTrue(result);
+        boolean result1 = StudentRegisterActivity.usernameValidate("test");
+        assertFalse(result1);
     }
 }
